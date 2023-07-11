43aad470726fc7f6c08ce4e72b54f61c7ccbfc07
moses-Desktop <moseschanx@gmail.com>
2023-07-11 18:16:19 +0800

    update : ignore Jlinksettings.ini

---
+++ MDK/RTE/_Main/RTE_Components.h
@@ -0,0 +1,22 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'stm32_stdPeriph_freertos' 
+ * Target:  'Main' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+#define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
+          #define RTE_Compiler_IO_STDOUT_BKPT     /* Compiler I/O: STDOUT Breakpoint */
+
+#endif /* RTE_COMPONENTS_H */
