7ff537406b6d9e6c5f8301fcee1f80ef3a6e1615
moses-Desktop <moseschanx@gmail.com>
2023-10-06 00:30:20 +0800

    added adc testing code

--- USER/main.c
+++ USER/main.c
@@ -4,6 +4,10 @@ static void
 dummy_task1(void* args __attribute((unused)) );
 static void 
 dummy_task2(void* args __attribute((unused)) );
+static void
+adc_channel_aquire_sample(void* args __attribute((unused)));
+static void
+adc_junction_temp_aquire(void* args __attribute((unused)));
 
 
 int main()
@@ -23,6 +27,8 @@ int main()
 
 
  rtt_printf("System Initialization End.\n");
+
+ SWO_PrintString("Test printing\n");
 	 
 
 
@@ -33,8 +39,11 @@ int main()
    */
 
 
-   xTaskCreate(dummy_task1,"dummy taks1",configMINIMAL_STACK_SIZE*5,NULL,tskIDLE_PRIORITY,NULL);
-   xTaskCreate(dummy_task2,"dummy taks2",configMINIMAL_STACK_SIZE*5,NULL,tskIDLE_PRIORITY,NULL);
+//   xTaskCreate(dummy_task1,"dummy taks1",configMINIMAL_STACK_SIZE*5,NULL,tskIDLE_PRIORITY,NULL);
+//   xTaskCreate(dummy_task2,"dummy taks2",configMINIMAL_STACK_SIZE*5,NULL,tskIDLE_PRIORITY,NULL);
+//   xTaskCreate(adc_channel_aquire_sample,"adc_inner_task",configMINIMAL_STACK_SIZE*5,NULL,tskIDLE_PRIORITY,NULL);
+   xTaskCreate(adc_junction_temp_aquire,"junction_temp",configMINIMAL_STACK_SIZE*5,NULL,tskIDLE_PRIORITY,NULL);
+
 
    vTaskStartScheduler();
 
@@ -59,6 +68,7 @@ dummy_task1(void* args __attribute((unused)) )
          {
 
             rtt_printf(malloc_test);
+            SWO_PrintString(malloc_test);
             delay_ms(2000);
          }
 
@@ -77,8 +87,120 @@ dummy_task2(void* args __attribute((unused)) )
          {
 
             rtt_printf(malloc_test);
+            SWO_PrintString(malloc_test);
             delay_ms(2000);
          }
 
 }
 
+static void
+adc_channel_aquire_sample(void* args __attribute((unused)))
+{
+   RCC_ADCCLKConfig(RCC_PCLK2_Div8);  
+  /* Enable ADC1 and GPIOC clock */
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1 | RCC_APB2Periph_GPIOC, ENABLE);
+  GPIO_InitTypeDef GPIO_InitStructure;
+
+  /* Configure PC.00 (ADC1 Channel10) as analog input -------------------------*/
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;
+  GPIO_Init(GPIOC, &GPIO_InitStructure);
+
+  ADC_InitTypeDef ADC_InitStructure;
+
+    /* ADC1 configuration ------------------------------------------------------*/
+  ADC_InitStructure.ADC_Mode = ADC_Mode_Independent;
+  ADC_InitStructure.ADC_ScanConvMode = ENABLE;
+  ADC_InitStructure.ADC_ContinuousConvMode = ENABLE;
+  ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
+  ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+  ADC_InitStructure.ADC_NbrOfChannel = 1;
+  ADC_Init(ADC1, &ADC_InitStructure);
+
+  /* ADC1 regular channel14 configuration */ 
+  ADC_RegularChannelConfig(ADC1, ADC_Channel_10, 1, ADC_SampleTime_239Cycles5);
+
+  
+  /* Enable ADC1 */
+  ADC_Cmd(ADC1, ENABLE);
+
+  /* Enable ADC1 reset calibration register */   
+  ADC_ResetCalibration(ADC1);
+  /* Check the end of ADC1 reset calibration register */
+  while(ADC_GetResetCalibrationStatus(ADC1));
+
+  /* Start ADC1 calibration */
+  ADC_StartCalibration(ADC1);
+  /* Check the end of ADC1 calibration */
+  while(ADC_GetCalibrationStatus(ADC1));
+     
+  /* Start ADC1 Software Conversion */ 
+  ADC_SoftwareStartConvCmd(ADC1, ENABLE);
+
+      rtt_printf("INNER ADC AQUIRING : ADC0 Software Conversion Started\n");
+
+
+   for(;;)
+   {
+   static uint16_t val = 0;
+   val =  ADC_GetConversionValue(ADC1);
+      rtt_printf("adc_channel_aquire_sample : %d\n",val);
+         delay_ms(200);
+
+  }
+
+
+}
+
+static void
+adc_junction_temp_aquire(void* args __attribute((unused)))
+{
+   RCC_ADCCLKConfig(RCC_PCLK2_Div8);  
+  /* Enable ADC1 and GPIOC clock */
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1 , ENABLE);
+
+  ADC_InitTypeDef ADC_InitStructure;
+
+    /* ADC1 configuration ------------------------------------------------------*/
+  ADC_InitStructure.ADC_Mode = ADC_Mode_Independent;
+  ADC_InitStructure.ADC_ScanConvMode = ENABLE;
+  ADC_InitStructure.ADC_ContinuousConvMode = ENABLE;
+  ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
+  ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+  ADC_InitStructure.ADC_NbrOfChannel = 16;
+  ADC_Init(ADC1, &ADC_InitStructure);
+
+  /* ADC1 regular channel14 configuration */ 
+  ADC_RegularChannelConfig(ADC1, ADC_Channel_16, 1, ADC_SampleTime_239Cycles5);
+
+  ADC_TempSensorVrefintCmd(ENABLE);
+  
+  /* Enable ADC1 */
+  ADC_Cmd(ADC1, ENABLE);
+
+  /* Enable ADC1 reset calibration register */   
+  ADC_ResetCalibration(ADC1);
+  /* Check the end of ADC1 reset calibration register */
+  while(ADC_GetResetCalibrationStatus(ADC1));
+
+  /* Start ADC1 calibration */
+  ADC_StartCalibration(ADC1);
+  /* Check the end of ADC1 calibration */
+  while(ADC_GetCalibrationStatus(ADC1));
+     
+  /* Start ADC1 Software Conversion */ 
+  ADC_SoftwareStartConvCmd(ADC1, ENABLE);
+
+
+
+   for(;;)
+   {
+   static uint16_t val = 0;
+   val =  ADC_GetConversionValue(ADC1);
+      rtt_printf("JUNCTION TEMP AQUIRE: %d \n",val);
+         delay_ms(500);
+
+  }
+
+
+}
