a0f246539c680d16406736a9c912ca321efc9395
moses-Desktop <moseschanx@gmail.com>
2023-10-09 08:20:31 +0800

    Added geekforgeeks atos lib

---
+++ 3rd-Party/ftos.h
@@ -0,0 +1,10 @@
+#ifndef FTOS_H 
+#define FTOS_H
+
+#include "math.h"
+
+void reverse(char* str, int len);
+int intToStr(int x, char str[], int d);
+void ftoa(float n, char* res, int afterpoint);
+
+#endif
