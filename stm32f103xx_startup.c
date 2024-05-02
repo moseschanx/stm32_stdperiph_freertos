9dbda4cea7ee7e54d8166e8fd8ad684ff5cb46fd
moses-Laptop <moseschanx@gmail.com>
2024-05-02 18:51:43 +0800

    modified SRAM size for setting stack starting address.

--- stm32f103xx_startup.c
+++ stm32f103xx_startup.c
@@ -1,9 +1,9 @@
 #include <stdint.h>
 
 #define SRAM_START	 0x20000000U
-#define SRAM_SIZE 	 (20U * 1024U)
+#define SRAM_SIZE 	 (64U * 1024U)
 #define SRAM_END  	 (SRAM_START + SRAM_SIZE)
-#define STACK_START	 SRAM_START + 0x400U
+#define STACK_START	 SRAM_START + 0x8000U
 
 extern int main(void);
 extern void SystemInit (void);
