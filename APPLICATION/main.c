18394a20779edb762320f7ee369b41142f70ff6a
moses-MacBook <moseschanx@gmail.com>
2024-06-02 16:09:54 +0800

    Buffer test code, printf test code.

--- APPLICATION/main.c
+++ APPLICATION/main.c
@@ -1,6 +1,16 @@
 #include "common.h"
 
-void uart_printf(char* str, ...);
+void uart_printf(uint8_t* fmt , ...);
+
+#define buffer1_size  sizeof(buffer1)
+#define buffer2_size  sizeof(buffer2)
+
+const uint8_t buffer1[] = "Buffer 1 content abcdefg.";
+const uint8_t buffer2[] = "Buffer 2 content hijklmn.xx";
+uint8_t buffer1_satus, buffer2_satus;
+
+uint8_t rx_buffer1[buffer1_size] = {0};
+uint8_t rx_buffer2[buffer2_size] = {0};
 
 
 void MCU_Init(void)
@@ -30,9 +40,6 @@ dbg_printf("System Initialization End.\n");
 // SWO_PrintString("Test printing\n");
 
 
-  
-
-
    
   /* blinky subroutine hardware prepare */
 	  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
@@ -56,84 +63,76 @@ dbg_printf("System Initialization End.\n");
 	  GPIO_Init(GPIOD, &gpio_init);
 	  GPIO_ResetBits(GPIOA,GPIO_Pin_2);
 	
-
-   
   
 } 
-
-
-
-// int stdout_device(char *str) {
-//     //return rtt_printf("%s\n", str);
-//     uart_printf(str);
-// }
-
 
 int main()
 {
   
   MCU_Init();
   //delay_ms(62);
+// Buffer memory test.
+{
+// buffer 1 test 
+  dbg_printf("buffer1 size : %d\n",buffer1_size);
+  for(int i=0;i<buffer1_size;++i)
+  {
+    rx_buffer1[i] = (uint8_t)('0' + i);
+  }
+  dbg_printf("rx_buffer1 content :");
+  for(int i=0;i<buffer1_size;++i)
+  {
+    //uart_printchar(rx_buffer1[i]);
+    dbg_printf("%c",rx_buffer1[i]);
+  }
+  dbg_printf("\n");
+  
+// buffer 2 test
+  dbg_printf("buffer2 size : %d\n",buffer2_size);
+  for(int i=0;i<buffer2_size;++i)
+  {
+    rx_buffer2[i] = (uint8_t)('0' + i);
+  }
+  dbg_printf("rx_buffer2 content :");
+  for(int i=0;i<buffer2_size;++i)
+  {
+   // uart_printchar(rx_buffer2[i]);
+   dbg_printf("%c",rx_buffer2[i]);
+  }
+  dbg_printf("\n");
+}
+
 
   
 //  English_Font_test();
+  //LCD_Init();
  // Touch_Test();
+ #if USE_SGL
   sgl_main();
-
-
-  
-  
-  
+#endif
 
   //English_Font_test();
 
-
+   uint32_t i = 4294967295;
    while(1)
    {
 
   //Touch_Test();
+#if USE_SGL
         sgl_tick_inc(5);
         sgl_task_handler();
+#endif
 
   /* blinky */
-	// GPIO_ResetBits(GPIOA,GPIO_Pin_8);
-	// delay_ms(200);
-	// GPIO_SetBits(GPIOA,GPIO_Pin_8);
-	// delay_ms(200);
+	 GPIO_ResetBits(GPIOA,GPIO_Pin_8);
+	 delay_ms(20);
+	 GPIO_SetBits(GPIOA,GPIO_Pin_8);
+	 delay_ms(20);
 
-  // dbg_printf("Program main loop!\n");
-  // uart_print("Program main loop!\n");
-
-  // rtt_printf("Program main loop!\n");
+   dbg_printf("Program main loop! %d\n",i++);
 
 
-  /* USART Test */
-  //  USART_SendData(USART1,'X');
-  // /* Loop until the end of transmission */
-  // while (USART_GetFlagStatus(USART1, USART_FLAG_TC) == RESET);
  
    }
-	 
 
 }
-
-void uart_printf(char* str,...)
-{
-
-  va_list ParamList;
-
-    while(*str!='\0')
-    {
-      while(USART_GetFlagStatus(USART1,USART_FLAG_TXE) == RESET); // If dont wait TXE, The first character might not sent through.
-      USART_SendData(USART1,*str++);
-      while(USART_GetFlagStatus(USART1,USART_FLAG_TC) == RESET);
-    }
-
-  va_end(ParamList);
-
-
-}
-
-
-
-
